64ff4e5d2cedd3a8b6e7885d2511276a883b75ce
Kimberly Schramm <kschramm@usgs.gov>
2017-06-22 09:08:58 -0600

    made minor updates to python code and cleaned up comments

--- checkorient.py
+++ checkorient.py
@@ -14,17 +14,24 @@ class Rotation:
         self.sttest=sttest
 
 # This function is used to rotate the data
-    def rot(self,theta):
+    def rotNS(self,theta):
         theta = theta % 360.
         cosd=np.cos(np.deg2rad(theta))
         sind=np.sin(np.deg2rad(theta))
         data1 = cosd*self.sttest[0].data -sind*self.sttest[1].data
-        data2 = sind*self.sttest[1].data + cosd*self.sttest[1].data
-        # Notice we are just checking the N/S we might want to add in the E/W
         resi = (abs(sum(data1*self.stref[0].data)/
                 np.sqrt(sum(data1**2)*sum(self.stref[0].data**2)) -1.))
         return resi
 
+# This function is used to rotate the data
+    def rotEW(self,theta):
+        theta = theta % 360.
+        cosd=np.cos(np.deg2rad(theta))
+        sind=np.sin(np.deg2rad(theta))
+        data2 = sind*self.sttest[1].data + cosd*self.sttest[1].data
+        resi = (abs(sum(data2*self.stref[1].data)/
+                np.sqrt(sum(data2**2)*sum(self.stref[1].data**2)) -1.))
+        return resi
 ########################################################################
 # start of the main program
 if __name__ == "__main__":
@@ -61,78 +68,8 @@ if __name__ == "__main__":
             day = str(ctime.julday).zfill(3)
             if debug:
                 print('On day: ' + day)
-<<<<<<< HEAD
-            try:
-				# format the string
-				string = '/msd/' + sta + '/' + str(ctime.year) + '/' + day + '/*LH*'
-				# read in the data
-				# Just grab one hour we might want to change this
-				st = read(string, starttime=ctime, endtime=ctime+60.*60)
-				if debug:
-					print(st)
-				st.detrend('demean')
-				st.merge()
-				st.filter('bandpass',freqmin=1./8., freqmax=1./4.)
-				st.taper(0.05)
-				# okay time to process the relative orientation
-				# We need to grab the different locations
-				locs = []
-				for tr in st:
-					locs.append(str(tr.stats.location))
-					locs = list(set(locs))
-					if debug:
-						print(locs)
-					# We now have all the location codes for the station
-					if len(locs) >= 2:
-					# We have at least two sensors so compare the azimuth
-					# First one will be the reference
-						refloc = locs.pop(0)
-						stref = st.select(location=refloc)
-						for loc in locs:
-							sttest = st.select(location=loc)
-							if debug:
-								print(stref)
-								print(sttest)
-							# This is really bad programming we need to fix this    
-							# it is really bad.  what the hell is he doing here?
-							# I got Austin to help and rewrote this as a class!
-			
-							#rotdata is an object that stores the data and has
-							#the rotation method.
-							rotdata=Rotation(stref,sttest)
-							#root function - finds the roots of the rotation
-							#method. lm is the levenberg-marquardt method
-							print('here')
-							result = root(rotdata.rot, 0., method = 'lm')
-							#not sure what this line is doing.
-							theta = result['x'][0]
-							#what is fun?
-							resi = result['fun'] 
-							print(result)
-							if debug:
-								print('Here is theta: ' + str(theta))
-								print('Here is the residual: ' + str(resi))
-							if 'f' not in globals():
-								f=open('Results_' + sta, 'w')
-								f.write('ReferenceLoc, TestLoc, day, year, theta, residual\n')
-							f.write(refloc +', '+ loc +', '+ day +', ' + str(ctime.year) + ', ' + str(theta) + ', ' + str(resi) + '\n')
-					
-            except:
-                print('Problem with ' + sta + ' on day ' + day)
-
-        #out of the try/except loop
-            ctime += 24.*60.*60.
-        #out of the station loop - move on to next station...
-        ctime = stime
-        # close the old file
-=======
-            #try:
-        #if True:
         # format the string
             string = '/msd/' + sta + '/' + str(ctime.year) + '/' + day + '/*LH*'
-        # check for data before reading in:
-        #    if not os.path.isfile(string):
-        #        continue
         # read in the data
         # Just grab one hour we might want to change this
             try:
@@ -172,12 +109,14 @@ if __name__ == "__main__":
                         rotdata=Rotation(stref,sttest)
             #root function - finds the roots of the rotation
             #method. lm is the levenberg-marquardt method
-                        print('here')
-                        result = root(rotdata.rot, 0., method = 'lm')
-            #not sure what this line is doing.
-                        theta = result['x'][0]
-            #what is fun?
-                        resi = result['fun'] 
+                        resultNS = root(rotdata.rotNS, 0., method = 'lm')
+                        resultEW = root(rotdata.rotEW, 0., method = 'lm')
+            #grab the results from the minimization problem
+                        thetaNS = resultNS['x'][0]
+                        thetaEW = resultEW['x'][0]
+            #This is the value of the residual function you are minimizing
+                        resiNS = resultNS['fun'] 
+                        resiEW = resultEW['fun'] 
                         print('result: ',str(result))
                         if debug:
                             print('Here is theta: ' + str(theta))
@@ -197,10 +136,9 @@ if __name__ == "__main__":
         ctime = stime
 
         # close the file when you exit the while loop
->>>>>>> notry
+
         if 'f' in globals():
             f.close()
 
-                    #sys.exit()
         #    except:
         #        print('Problem with ' + sta + ' on day ' + day)
